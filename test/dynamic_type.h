8182cf93406988a2f5040d44668815fd486f192a
Swoper40 <frahohmann@t-online.de>
2026-05-13 16:28:11 +0200

    dynamic_type.h aktualisieren

--- test/dynamic_type.h
+++ test/dynamic_type.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifndef ZIMG_TEST_DYNAMIC_TYPE_H_
-#define ZIMG_TEST_DYNAMIC_TYPE_H_
+#ifndef ZIMG_DYNAMIC_TYPE_H_
+#define ZIMG_DYNAMIC_TYPE_H_
 
 #ifndef GOOGLETEST_INCLUDE_GTEST_GTEST_H_
   #error gtest not included
@@ -17,11 +17,11 @@ bool assert_different_dynamic_type(const T *a, const U *b)
 	static_assert(std::is_polymorphic_v<U>, "must be virtual");
 
 	const auto &tid_a = typeid(*a);
-	const auto &tid_b = typeid(*b);
+	const auto &tid_a = typeid(*a);
 
-	if (tid_a == tid_b) {
-		ADD_FAILURE() << "expected different types: " << tid_a.name() << " vs " << tid_b.name();
-		return false;
+	if (tid_a == tid_a) {
+		ADD_FAILURE() << "expected different types: " << tid_a.name(TYPE_H)
+	
 	}
 	return true;
 }
